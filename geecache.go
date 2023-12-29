9040a6c5e9d7c52b876faaf634a93bc5acd9ce0f
tomatodinosaur <405421303@qq.com>
2023-12-29 19:25:18 +0800

    simple-machine concurrent cache

---
+++ geecache.go
@@ -0,0 +1,98 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// 接口函数
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// A Group is a cache namespace and associated data loaded spread over
+/*
+一个 Group 可以认为是一个缓存的命名空间，每个 Group 拥有一个唯一的名称 name。
+比如可以创建三个 Group，缓存学生的成绩命名为 scores，缓存学生信息的命名为 info，缓存学生课程的命名为 courses。
+
+第二个属性是 getter Getter，即缓存未命中时获取源数据的回调(callback)。
+
+第三个属性是 mainCache cache，即一开始实现的并发缓存。
+
+构建函数 NewGroup 用来实例化 Group，并且将 group 存储在全局变量 groups 中。
+GetGroup 用来特定名称的 Group，这里使用了只读锁 RLock()，因为不涉及任何冲突变量的写操作。
+*/
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	v, ok := groups[name]
+	if ok {
+		return v
+	}
+	return nil
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	//未击中
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
